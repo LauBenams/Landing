32525a3a829dd87217ea14bfff4c4529471709bd
LauBenams <laurence@pinpo.fr>
2015-03-10 18:16:01 +0100

    changements steps

--- app/api/config.inc.php
+++ app/api/config.inc.php
@@ -1,7 +1,8 @@
 <?php
+// define('_DB_SERVER_', 'pinpofrhaer.mysql.db');
 define('_DB_SERVER_', '176.31.249.146');
-define('_DB_NAME_', 'pinpofrhae_root_');
-define('_DB_USER_', 'pinpofrhae_root_');
+define('_DB_NAME_', 'pinpofrhaer');
+define('_DB_USER_', 'pinpofrhaer');
 define('_DB_PASSWD_', 'Lo6Bg4htXCgV');
 
 define('_SMTP_SERVER_', 'smtp.pinpo.fr');
